4b6cd0831aee9c400ae38dbe1a45388b9cb18c4e
CleilsonAndrade <cleilsonjose@hotmail.com>
2024-03-28 10:37:08 -0300

    🐞 fix: repository identify type

--- src/main/java/br/com/cleilsonandrade/challengeregistergame/repositories/PlayerRepository.java
+++ src/main/java/br/com/cleilsonandrade/challengeregistergame/repositories/PlayerRepository.java
@@ -1,13 +1,11 @@
 package br.com.cleilsonandrade.challengeregistergame.repositories;
 
-import java.util.UUID;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.cleilsonandrade.challengeregistergame.model.Player;
 
 @Repository
-public interface PlayerRepository extends JpaRepository<Player, UUID> {
+public interface PlayerRepository extends JpaRepository<Player, Long> {
 
 }
